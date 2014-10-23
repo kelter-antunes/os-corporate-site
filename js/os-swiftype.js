3bb08b6bd8cd95d2303012d5bc845f5e840d6dac
kelter-antunes <miguel.aka.kelter@gmail.com>
2014-10-23 11:33:27 +0100

    st autocomplete

--- js/os-swiftype.js
+++ js/os-swiftype.js
@@ -28,86 +28,86 @@ $(function() {
         else{
             out = out.concat('<p class="body">' + item.body.substring(0, 200).replace('matchCookie',' ').replace('<em>match</em>Cookie','') + '...' + '</p>');
         }
-          /*  
-        if (item.highlight.sections) {
-            var i = '<span class="section">' + item.highlight.sections + "</span>";
-            out = out.concat('<p class="sections">' + i + "</p>");
-        }*/
+/*  
+if (item.highlight.sections) {
+var i = '<span class="section">' + item.highlight.sections + "</span>";
+out = out.concat('<p class="sections">' + i + "</p>");
+}*/
 
-        return out;
-    };
+return out;
+};
 
 
 
 var customResultRenderFunction = function(ctx, data) {
-        var WebSite = [];
+    var WebSite = [];
 
-        var urlstr = '/search/#stq=' + $("#st-search-input").val() + '&stp=1';
+    var urlstr = '/search/#stq=' + $("#st-search-input").val() + '&stp=1';
 
-        var item_all_res = {"type": "post",
-            "url": urlstr,
-            "title": "",
-            "id": "",
-            "_type": "page",
-            "_index": "crawled",
-            "_version": null,
-            "sort": null,
-            "highlight": {
-              "title": ""
-            },
+    var item_all_res = {"type": "post",
+    "url": urlstr,
+    "title": "",
+    "id": "",
+    "_type": "page",
+    "_index": "crawled",
+    "_version": null,
+    "sort": null,
+    "highlight": {
+        "title": ""
+    },
+};
+
+
+$.each(data, function(docType, results) {
+    $.each(results, function(idx, result) {
+        if(result.type && WebSite.length < 5){
+            WebSite.push(result);
+            return;
         }
-
-
-        $.each(data, function(docType, results) {
-            $.each(results, function(idx, result) {
-                if(result.type && WebSite.length < 5){
-                      WebSite.push(result);
-                      return;
-                  }
-        });
-      });
-
-    var badruz = $('<div class="autocompbox"></div>');
-
-    var WebSiteList = $('<ul class="WebSite"></ul>');
-
-
-    $.each(WebSite, function(idx, item) {
-        var out = '<p class="title">' + item['title'] + '</p>';
-        if (item['description'] !== undefined) {
-            out = out.concat('<p class="body">' + item['description'].replace('matchCookie',' ').replace('<em>match</em>Cookie','') + "</p>");
-        }
-        else if(item.highlight['body'] !== undefined){
-            out = out.concat('<p class="body">' + item.highlight['body'].replace('matchCookie',' ').replace('<em>match</em>Cookie','') + '...' + "</p>");
-        }
-        else{
-            out = out.concat('<p class="body">' + item.body.substring(0, 200).replace('matchCookie',' ').replace('<em>match</em>Cookie','') + '...' + '</p>');
-        }
-        ctx.registerResult($('<li class="result">' + out + '</li>').appendTo(WebSiteList), item);
     });
-    if(WebSite.length>0){
-        ctx.registerResult($('<li class="swtpbutton"><p>See all results</p></li>').appendTo(WebSiteList),item_all_res);
-        WebSiteList.appendTo(badruz);
+});
+
+var badruz = $('<div class="autocompbox"></div>');
+
+var WebSiteList = $('<ul class="WebSite"></ul>');
+
+
+$.each(WebSite, function(idx, item) {
+    var out = '<p class="title">' + item['title'] + '</p>';
+    if (item['description'] !== undefined) {
+        out = out.concat('<p class="body">' + item['description'].replace('matchCookie',' ').replace('<em>match</em>Cookie','') + "</p>");
     }
-
-
-    if(WebSite.length > 0){
-        badruz.appendTo(ctx.list);
+    else if(item.highlight['body'] !== undefined){
+        out = out.concat('<p class="body">' + item.highlight['body'].replace('matchCookie',' ').replace('<em>match</em>Cookie','') + '...' + "</p>");
     }
+    else{
+        out = out.concat('<p class="body">' + item.body.substring(0, 200).replace('matchCookie',' ').replace('<em>match</em>Cookie','') + '...' + '</p>');
+    }
+    ctx.registerResult($('<li class="result">' + out + '</li>').appendTo(WebSiteList), item);
+});
+if(WebSite.length>0){
+    ctx.registerResult($('<li class="swtpbutton"><p>See all results</p></li>').appendTo(WebSiteList),item_all_res);
+    WebSiteList.appendTo(badruz);
+}
 
-  };
+
+if(WebSite.length > 0){
+    badruz.appendTo(ctx.list);
+}
+
+};
 
 
 var customResultStyle = function($this) {
-        var styles = {
-            'position': 'absolute',
-            'z-index': 9999,
-            'top': '80px',
-            'left': 'auto',
-            'width': '980px'
-        };
-        return styles;
+    var styles = {
+        'position': 'absolute',
+        'z-index': 9999,
+        'top': '80px',
+        'left': 'auto',
+        'width': '980px'
     };
+    return styles;
+};
 
 
 $("#st-search-input").keydown(function(ev) {
@@ -129,17 +129,17 @@ $("#st-search-input-2").keydown(function(ev) {
 
 function isElementInViewport (el) {
 
-    //special bonus for those using jQuery
-    if (typeof jQuery === "function" && el instanceof jQuery) {
-        el = el[0];
-    }
-    var btn_h = $('.swtpbutton')[0].getBoundingClientRect();
-    var rect = el.getBoundingClientRect();
+//special bonus for those using jQuery
+if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+}
+var btn_h = $('.swtpbutton')[0].getBoundingClientRect();
+var rect = el.getBoundingClientRect();
 
-    return (
-        rect.left >= 0 &&
-        rect.bottom + btn_h.height <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+return (
+    rect.left >= 0 &&
+    rect.bottom + btn_h.height <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
     );
 }
 
@@ -151,19 +151,19 @@ function fireIfElementVisible () {
         if($('.result').last()[0] !== undefined){
             if ( isElementInViewport($('.result').last())) {
                 $('.swtpbutton').css({'position': 'initial', 'bottom': 'initial', 'width':'auto' ,
-                                        'padding-left': '0px' , 'padding-right': '0px' ,
-                                         'border-left': '0px solid #e6e6e6' ,
-                                         'border-bottom': '0px solid #e6e6e6'});
+                    'padding-left': '0px' , 'padding-right': '0px' ,
+                    'border-left': '0px solid #e6e6e6' ,
+                    'border-bottom': '0px solid #e6e6e6'});
             }
             else{
                 var wdth = $('.result').last()[0].getBoundingClientRect().width;
                 $('.swtpbutton').css({'position':'fixed','bottom':'0','width':wdth ,
-                                 'padding-left': '0px' , 'padding-right': '0px' ,
-                                 'border-left': '1px solid #e6e6e6' ,
-                                 'border-bottom': '1px solid #e6e6e6'});
+                    'padding-left': '0px' , 'padding-right': '0px' ,
+                    'border-left': '1px solid #e6e6e6' ,
+                    'border-bottom': '1px solid #e6e6e6'});
             }
         }
-    }
+    };
 }
 
 
@@ -172,7 +172,7 @@ var handler = fireIfElementVisible() ;
 
 
 //jQuery
-$(window).on('DOMContentLoaded load resize scroll', handler); 
+$(window).on('DOMContentLoaded load resize scroll', handler);
 
 
 
@@ -195,23 +195,26 @@ $(window).hashchange(function() {
 
 
 $(document).delegate('.autocomplete li',"mouseout", function () {
-     $(this).removeClass('active');
+    $(this).removeClass('active');
 });
 
 
 
 
 //SEARCH***********************************************************
-
 $('#st-search-input').swiftypeSearch({
     resultContainingElement: '#st-results-container',
     engineKey: 'GZhgtDYXiyvDjz48t2SP',
     renderFunction: customRendererSearch,
     perPage: 10,
     resultPageURL: '/search/',
-    /*searchFields: {
-        'page': ['body^2', 'title^2.5', 'sections^3.5', 'tags^1.5', 'url^1']
-    }*/
+    filters: function() {
+        return {
+            'page': {
+                'type': ['website', 'blog']
+            }
+        };
+    }
 });
 
 
@@ -219,12 +222,9 @@ $('#st-search-input').swiftypeSearch({
 $('#st-search-input').swiftype({
     engineKey: 'GZhgtDYXiyvDjz48t2SP',
     resultRenderFunction: customResultRenderFunction,
-    //setWidth: '980px',
     setWidth: false,
     resultLimit: 5,
-    fetchFields: {page: ['url', 'body', 'title', 'type', 'highlight', 'sections']},
-    //autocompleteContainingElement: '.navigation-inner.container',
-    //dropdownStylesFunction: customResultStyle
+    fetchFields: {page: ['body', 'title', 'highlight']},
     filters: function() {
         return {
             'page': {
@@ -236,14 +236,10 @@ $('#st-search-input').swiftype({
 });
 
 
-
 $('#st-search-input-2').swiftype({
     engineKey: 'GZhgtDYXiyvDjz48t2SP',
-    //resultRenderFunction: customResultRenderFunction,
-    //setWidth: false,
-    fetchFields: {page: ['url', 'body', 'title', 'type', 'highlight', 'sections']},
+    fetchFields: {page: ['body', 'title', 'highlight']},
     renderFunction: customRenderAutoComplete,
-    //setWidth: false,
     resultLimit : 5,
     filters: function() {
         return {
