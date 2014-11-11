0c63890dc47ac866d79cd2b58b730456f98a6119
ZZaRpp <neozarp@gmail.com>
2014-11-11 15:45:29 +0000

    swiftype - no results page

--- js/jquery.swiftype.search.js
+++ js/jquery.swiftype.search.js
@@ -244,7 +244,10 @@
         }
 
         if (totalResultCount === 0) {
-            $resultContainer.html("<div id='st-no-results' class='st-no-results'>No results found.</div>");
+            //$resultContainer.html("<div id='st-no-results' class='st-no-results'>No results found.</div>");
+            $resultContainer.find('#st-no-results').css({display:'block'});
+        }else{
+            $resultContainer.find('#st-no-results').css({display:'none'});
         }
 
         if (spellingSuggestion !== null) {
