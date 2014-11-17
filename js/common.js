fd4aab94f8b765d363c7ec7f1b321047c46df30e
kelter <miguel.aka.kelter@gmail.com>
2014-11-17 17:40:27 +0000

    kiss - mkto

--- js/common.js
+++ js/common.js
@@ -170,23 +170,27 @@ var messageEvent_Global = eventMethod_Global == "attachEvent" ? "onmessage" : "m
 eventer_Global(messageEvent_Global, function(e) {
     if (e.data !== "") {
 
-        var pathName = e.data.srcElement.location.href
+        if (e.data === 'kissEvent') {
+            var pathName = e.data.srcElement.location.href;
 
-        var isAppContactUS = (pathName === '/contact-us-apps/');
-        var isGlobalContactUS = (pathName === '/contact-us-global/');
-        var isOfficeContactUS = (pathName === '/company/contact-us/');
+            var isAppContactUS = (pathName === '/contact-us-apps/');
+            var isGlobalContactUS = (pathName === '/contact-us-global/');
+            var isOfficeContactUS = (pathName === '/company/contact-us/');
 
 
-        if (isAppContactUS) {
-            trackEvent('CS - Submit Contact US Apps');
+            if (isAppContactUS) {
+                trackEvent('CS - Submit Contact US Apps');
 
-        } else if (isGlobalContactUS) {
-            trackEvent('CS - Submit Contact US Global');
+            } else if (isGlobalContactUS) {
+                trackEvent('CS - Submit Contact US Global');
 
-        } else if (isOfficeContactUS) {
-            trackEvent('CS - Submit Contact US Offices');
+            } else if (isOfficeContactUS) {
+                trackEvent('CS - Submit Contact US Offices');
 
-        }
+            }
+
+        };
+
 
     }
 }, false);
