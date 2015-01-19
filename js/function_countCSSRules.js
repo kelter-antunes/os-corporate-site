b75a16309ea0cf5d8f6b5ffd9ad6a55bcf5c4a95
kelter <miguel.aka.kelter@gmail.com>
2015-01-19 11:58:58 +0000

    app fit copy

---
+++ js/function_countCSSRules.js
@@ -0,0 +1,36 @@
+function countCSSRules() {
+    var results = '',
+        log = '';
+    if (!document.styleSheets) {
+        return;
+    }
+    for (var i = 0; i < document.styleSheets.length; i++) {
+        countSheet(document.styleSheets[i]);
+    }
+    function countSheet(sheet) {
+        var count = 0;
+        if (sheet && sheet.cssRules) {
+            for (var j = 0, l = sheet.cssRules.length; j < l; j++) {
+                var rule = sheet.cssRules[j];
+                if (rule instanceof CSSImportRule) {
+                    countSheet(rule.styleSheet);
+                }
+                if( !rule.selectorText ) {
+                    continue;
+                }
+                count += rule.selectorText.split(',').length;
+            }
+
+            log += '\nFile: ' + (sheet.href ? sheet.href : 'inline <style> tag');
+            log += '\nRules: ' + sheet.cssRules.length;
+            log += '\nSelectors: ' + count;
+            log += '\n--------------------------';
+            if (count >= 4096) {
+                results += '\n********************************\nWARNING:\n There are ' + count + ' CSS rules in the stylesheet ' + sheet.href + ' - IE will ignore the last ' + (count - 4096) + ' rules!\n';
+            }
+        }
+    }
+    console.log(log);
+    console.log(results);
+};
+countCSSRules();
