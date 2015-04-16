fbee421be487dc1ae97aa7070eafd2fe48b443ba
ZZaRpp <neozarp@gmail.com>
2015-04-16 18:17:03 +0100

    search mobile

--- js/os-swiftype.js
+++ js/os-swiftype.js
@@ -174,7 +174,7 @@ $(function() {
         if (ev.which === 13 && !$('.autocomplete li.active').is(':visible')) {
             window.location = '/search/#stq=' + $(this).val() + '&stp=1';
             $("#st-search-input-2").val($(this).val());
-            event.stopPropagation();
+            ev.stopPropagation();
         }
     });
 
@@ -183,7 +183,7 @@ $(function() {
         if (ev.which === 13 && !$('.autocomplete li.active').is(':visible')) {
             window.location.hash = '#stq=' + $(this).val() + '&stp=1';
             $("#st-search-input").val($(this).val());
-            event.stopPropagation();
+            ev.stopPropagation();
         }
     });
 
@@ -197,7 +197,7 @@ $(function() {
     });
 
 
-    $(window).hashchange(function(event) {
+    $(window).hashchange(function() {
              if ($.hashParams().stq !== "") {
                 $("#st-search-input").val($.hashParams().stq);
                 $("#st-search-input-2").val($.hashParams().stq);
