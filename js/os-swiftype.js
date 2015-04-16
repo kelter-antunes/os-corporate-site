e08e7976936672f25973016e394435544720414f
ZZaRpp <neozarp@gmail.com>
2015-04-16 18:39:37 +0100

    search mobile

--- js/os-swiftype.js
+++ js/os-swiftype.js
@@ -230,21 +230,6 @@ $(function() {
     });
 
 
-    $('#st-search-input-2').swiftypeSearch({
-        resultContainingElement: '#st-results-container',
-        engineKey: 'GZhgtDYXiyvDjz48t2SP',
-        renderFunction: customRendererSearch,
-        perPage: 10,
-        resultPageURL: '/search/'
-            /*,
-            filters: function() {
-                return {
-                    'page': {
-                        'type': ['website', 'blog']
-                    }
-                };
-            }*/
-    });
 
     //AUTOCOMPLETE***************************************************
     $('#st-search-input').swiftype({
