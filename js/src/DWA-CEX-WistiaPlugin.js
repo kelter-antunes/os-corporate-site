76244f80979decf62f426e065fa0bdfe8875a070
kelter <miguel.aka.kelter@gmail.com>
2015-09-30 13:43:24 +0100

    fix

--- js/src/DWA-CEX-WistiaPlugin.js
+++ js/src/DWA-CEX-WistiaPlugin.js
@@ -38,40 +38,48 @@ Wistia.plugin("dwa_cex", function(video, options) {
 
     pushEvent = function(action, name, id) {
 
+        var ebRand = Math.random() + '';
+        ebRand = ebRand * 1000000;
+
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+
         if (id == 'k25yv07jjj') {
             // #13 Conversion Name: OS MADP - Video - The “Yes” Platform from OutSystems.
-            var ebRand = Math.random() + '';
-            ebRand = ebRand * 1000000;
             //document.write('<scr' + 'ipt src="HTTPS://bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=677085&amp;rnd=' + ebRand + '"></scr' + 'ipt>');
 
-            var script = document.createElement("script");
-            script.type = "text/javascript";
-            script.src = "HTTPS://bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=677085&amp;rnd=" + ebRand; 
+            script.src = "HTTPS://bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=677085&amp;rnd=" + ebRand;
             document.body.appendChild(script);
 
         } else if (id == 'cylzp3c4b6') {
             // #9 Conversion Name: OS MADP - Video - Deploy Automatically
-            var ebRand = Math.random() + '';
-            ebRand = ebRand * 1000000;
-            document.write('<scr' + 'ipt src="HTTPS://bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=677081&amp;rnd=' + ebRand + '"></scr' + 'ipt>');
+            //document.write('<scr' + 'ipt src="HTTPS://bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=677081&amp;rnd=' + ebRand + '"></scr' + 'ipt>');
+
+            script.src = "HTTPS://bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=677081&amp;rnd=" + ebRand;
+            document.body.appendChild(script);
 
         } else if (id == 'kyfexwxc7w') {
             // #8 Conversion Name: OS MADP - Video - Change Continuously
-            var ebRand = Math.random() + '';
-            ebRand = ebRand * 1000000;
-            document.write('<scr' + 'ipt src="HTTPS://bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=677082&amp;rnd=' + ebRand + '"></scr' + 'ipt>');
+            //document.write('<scr' + 'ipt src="HTTPS://bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=677082&amp;rnd=' + ebRand + '"></scr' + 'ipt>');
+
+            script.src = "HTTPS://bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=677082&amp;rnd=" + ebRand;
+            document.body.appendChild(script);
 
         } else if (id == 'n4a6wudc6t') {
             // #11 Conversion Name: OS MADP - Video - Manage Centrally
-            var ebRand = Math.random() + '';
-            ebRand = ebRand * 1000000;
-            document.write('<scr' + 'ipt src="HTTPS://bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=677083&amp;rnd=' + ebRand + '"></scr' + 'ipt>');
+            //document.write('<scr' + 'ipt src="HTTPS://bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=677083&amp;rnd=' + ebRand + '"></scr' + 'ipt>');
+
+            script.src = "HTTPS://bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=677083&amp;rnd=" + ebRand;
+            document.body.appendChild(script);
+
+
 
         } else if (id == '67sdl1xgu9') {
             // #10 Conversion Name: OS MADP - Video - Develop Visually
-            var ebRand = Math.random() + '';
-            ebRand = ebRand * 1000000;
-            document.write('<scr' + 'ipt src="HTTPS://bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=677080&amp;rnd=' + ebRand + '"></scr' + 'ipt>');
+            //document.write('<scr' + 'ipt src="HTTPS://bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=677080&amp;rnd=' + ebRand + '"></scr' + 'ipt>');
+
+            script.src = "HTTPS://bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=677080&amp;rnd=" + ebRand;
+            document.body.appendChild(script);
 
         }
 
